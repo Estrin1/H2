73aeff2a2e2e985aedb39afc07067e6fedc07200
Evgeniy <estrin.j@yandex.ru>
2022-11-19 10:28:05 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Выводит только третью цифру заданного числа, или пишет что ее нет
+//
+
+
+
+Console.WriteLine("Write the number");
+int a = Convert.ToInt32(Console.ReadLine());
+int num = a;
+
+
+
+if (a < 100)
+{
+    Console.WriteLine("I no have 3rd number -> " + a);
+    
+}
+
+ if  (a>100 & a<1000)
+{
+    a = a % 10;
+    Console.WriteLine(a);
+}
+while ( a >999 ) 
+{
+    a = a / 10;
+  
+}  
+if (a >100)
+{
+num=a % 10;
+Console.WriteLine(num);
+}
+
