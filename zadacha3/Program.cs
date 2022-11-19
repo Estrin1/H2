9b499c9fe9ca181e4f15cf5495139a97d4eedd09
Evgeniy <estrin.j@yandex.ru>
2022-11-19 13:26:40 +0300

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ВВедите цифру от 1 до 7");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 == 6 | num1 == 7)
+{
+  Console.WriteLine("ВЫХОДНЫЕ > YES " + num1);
+}
+else
+{
+  Console.WriteLine("Будни > NO " + num1);  
+}
