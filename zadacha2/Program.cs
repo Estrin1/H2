26b1482fe89b877c5a4538b8bd2197d1c0336719
Evgeniy <estrin.j@yandex.ru>
2022-11-19 10:31:20 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+void zadacha1()
+{
+int num = 0, num2 = 0; 
+int num3;
+Console.WriteLine("Введите число");
+int num1 =  Convert.ToInt32(Console.ReadLine());
+num = num1 % 100;
+num2 = num1% 10;
+num3= (num - num2)/10;
+Console.WriteLine(num3);  
+}
+zadacha1();
